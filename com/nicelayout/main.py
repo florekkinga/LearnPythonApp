51f2c805ed4f3831b3d03326752155ac217a7b2b
florekkinga <florek.kinga@gmail.com>
2020-06-15 15:00:02 +0200

    added new buttons and "tryb kariera" screen

--- com/nicelayout/main.py
+++ com/nicelayout/main.py
@@ -8,7 +8,7 @@ from kivy.core.window import Window
 from kivy.lang import Builder
 from kivy.config import Config
 
-Window.clearcolor = (0.67, 1, 0.94, 1)
+Window.clearcolor = (210 / 255, 243 / 255, 236 / 255, 1)
 # Window.size = (800, 600)
 kivy.require("1.11.1")
 
@@ -32,10 +32,24 @@ class Nauka(Screen):
 class Powtorka(Screen):
     pass
 
+
 class Ranking(Screen):
     pass
 
+
 class MojeKonto(Screen):
+    pass
+
+
+class Easy(Screen):
+    pass
+
+
+class Medium(Screen):
+    pass
+
+
+class Hard(Screen):
     pass
 
 
@@ -48,6 +62,7 @@ class ImageButton(ButtonBehavior, Image):
 
 
 kv = Builder.load_file("nice.kv")
+
 
 class NiceApp(App):
 
